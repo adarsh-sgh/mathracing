42a7c0b7b080265666a5780e2beb5f5ebeeaa8cf
LAKKI <thakurluckysinghbrh@gmail.com>
2021-06-04 10:37:32 +0530

    err message logged

--- math.js
+++ math.js
@@ -244,7 +244,7 @@ if ('serviceWorker' in navigator) {
       navigator.serviceWorker.register('/sw.js').then(function (registration) {
          // console.log('ServiceWorker registration successful with scope: ', registration.scope);
       }, function (err) {
-         // console.log('ServiceWorker registration failed: ', err);
+         console.log('ServiceWorker registration failed: ', err);
       });
    });
 }
